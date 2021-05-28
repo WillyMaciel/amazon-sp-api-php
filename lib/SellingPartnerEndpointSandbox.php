c64a6b1c2781481dd7d3687f3399495c71f80ed3
Willy Bernardes Maciel <willy_maciel@live.com>
2021-05-28 16:49:01 -0300

    Adicionado Endpoints de Sandbox

---
+++ lib/SellingPartnerEndpointSandbox.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ClouSale\AmazonSellingPartnerAPI;
+
+class SellingPartnerEndpointSandbox
+{
+    public static $EUROPE = 'https://sandbox.sellingpartnerapi-eu.amazon.com';
+    public static $FAR_EAST = 'https://sandbox.sellingpartnerapi-fe.amazon.com';
+    public static $NORTH_AMERICA = 'https://sandbox.sellingpartnerapi-na.amazon.com';
+}
